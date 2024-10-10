d35f3447a5337c530d74893d042a15bf793afff9
jimbjo <jimmy.bjorkstrand@xlent.se>
2024-10-10 10:41:40 +0000

    commit

--- dbtlearn/models/src/src_hosts.sql
+++ dbtlearn/models/src/src_hosts.sql
@@ -1,6 +1,6 @@
 
 with raw_hosts AS (
-    SELECT * FROM AIRBNB.RAW.RAW_HOSTS
+    SELECT * FROM {{ source('airbnb', 'hosts') }}
 )
 SELECT
     id as host_id
--- dbtlearn/models/src/src_reviews.sql
+++ dbtlearn/models/src/src_reviews.sql
@@ -1,6 +1,6 @@
 
 with raw_reviews AS (
-    SELECT * FROM AIRBNB.RAW.RAW_REVIEWS
+    SELECT * FROM {{ source('airbnb', 'reviews') }}
 )
 SELECT
     listing_id
